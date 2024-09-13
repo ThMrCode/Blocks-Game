4ab095318fd9c4a5c0047372a91511e57cece721
ThMrCode <thmrcode@gmail.com>
2024-09-13 05:31:25 -0500

    Subiendo

--- jugar/script.js
+++ jugar/script.js
@@ -4,6 +4,21 @@ const ctx = canvas.getContext('2d');
 const startButton = document.getElementById('startButton');
 const resetButton = document.getElementById('resetButton');
 
+// Recuerda, la ejecucion de una funcion asincrona es hacer su parte sincrona y añadir su parte asincrona como una microtarea a la cola una vez añadida, termina su ejecucion
+// Recuerda, cuando se termina una promesa, la ultima linea de esta es añadir su then o catch a la cola de microtareas
+
+fetch("../resources/blocks.json")
+    .then(response => {
+        if(!response.ok) throw new Error("Error al cargar bloques");
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+    })
+    .catch(err => {
+        throw new Error("Error al leer bloques");
+    })
+    
 function startGame() {
     console.log("Juego Iniciado");
 }
