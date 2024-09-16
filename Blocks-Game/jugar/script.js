0afcf8d9334e9b9aea5fe87d3cbb8bb985aea5d5
ThMrCode <thmrcode@gmail.com>
2024-09-16 00:52:42 -0500

    Casi Finish

--- Blocks-Game/jugar/script.js
+++ Blocks-Game/jugar/script.js
@@ -4,6 +4,8 @@ const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 const startButton = document.getElementById('startButton');
 const resetButton = document.getElementById('resetButton');
+const sound = new Audio("../resources/sound.mp3");
+sound.loop=true;
 
 class Data {
     constructor() {
@@ -30,11 +32,14 @@ class Box {
     }
 }
 class Img {
-    constructor(x_,y_,shape_,id_) {
+    constructor(x_,y_,shape_,id_, color_, type_, rotate_) {
         this.x = x_;
         this.y = y_;
         this.shape = shape_;
         this.id = id_;
+        this.color = color_;
+        this.type = type_;
+        this.rotate = rotate_;
         this.sand = false;
     }
 }
@@ -52,6 +57,8 @@ class Utils {
         return -1;
     }
     static spawn_img(id, grid) {
+        // Spawnea una imagen aleatoria de las opciones en data, (type, rotate, color)
+        // La coloca en el grid (Boxs), y tambien retorna esta imagen
         let img_type = Math.floor(Math.random()*data.n_type);
         let img_rotate = Math.floor(Math.random()*data.n_rotate);
         let img_color = data.colors[Math.floor(Math.random()*data.n_color)];
@@ -63,7 +70,7 @@ class Utils {
                 }
             }
         }
-        return new Img(img_x,0,data.images[img_type][img_rotate],id);
+        return new Img(img_x, 0, data.images[img_type][img_rotate], id, img_color,img_type,img_rotate);
     }
     static verify_move_img(img, grid, dx, dy) {
         // Funcion que se encarga de ver si una imagen (solida) puede moverse 
@@ -91,6 +98,7 @@ class Utils {
         return move;
     }
     static move_img(img, grid, dx, dy) {
+        // Funcion que mueve una imagen (su x, y) y tambien sus Boxs en el grid
         let pots = [];
         let pots_delete = [];
         for (let i = 0; i < 4; i++) {
@@ -105,18 +113,78 @@ class Utils {
                 if(img.shape[i][j] == 1) {
                     let pot = {y: (img.y + i),x: (img.x + j)};
                     let index = this.index(pot,pots);
-                    if(index != -1) {
+                    if(index == -1) {
                         pots_delete.push(pot);
-                        pots.pop(index);
                     }
                 }
             }
         }
         for (let i = 0; i < pots.length; i++) {
-            grid[pots[i].y][pots[i].x] = new Box(img.id,)
+            grid[pots[i].y][pots[i].x] = new Box(img.id,img.color);
+        }
+        for (let i = 0; i < pots_delete.length; i++) {
+            grid[pots_delete[i].y][pots_delete[i].x] = new Box(0,data.bg_color);
         }
         img.x = img.x + dx;
         img.y = img.y + dy;
+    }
+    static verify_rotate_img(img, grid, rotate) {
+        // Funcion que verifica si una imagen puede rotar
+        let new_rotate = (img.rotate + rotate) % data.n_rotate;
+        let new_shape = data.images[img.type][new_rotate];
+        let move = true;
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if(new_shape[i][j] == 1) {
+                    let new_y = img.y + i;
+                    let new_x = img.x + j;
+                    if(this.verify_range(new_x,0,(data.grid_w-1)) && this.verify_range(new_y,0,(data.grid_h-1))) {
+                        if(grid[new_y][new_x].id != 0 && grid[new_y][new_x].id != img.id) {
+                            move = false;
+                            break;
+                        }
+                    }
+                    else {
+                        move = false;
+                        break;
+                    }
+                }
+            }
+        }
+        return move;
+    }
+    static rotate_img(img, grid, rotate) {
+        // Funcion que rota la imagen (su shape) y sus Boxs en la grid
+        let new_rotate = (img.rotate + rotate) % data.n_rotate;
+        let new_shape = data.images[img.type][new_rotate];
+        let pots = [];
+        let pots_delete = [];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if(new_shape[i][j] == 1) {
+                    pots.push({ y: (img.y + i), x: (img.x + j)});
+                }
+            }
+        }
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                if(img.shape[i][j] == 1) {
+                    let pot = {y: (img.y + i),x: (img.x + j)};
+                    let index = this.index(pot,pots);
+                    if(index == -1) {
+                        pots_delete.push(pot);
+                    }
+                }
+            }
+        }
+        for (let i = 0; i < pots.length; i++) {
+            grid[pots[i].y][pots[i].x] = new Box(img.id,img.color);
+        }
+        for (let i = 0; i < pots_delete.length; i++) {
+            grid[pots_delete[i].y][pots_delete[i].x] = new Box(0,data.bg_color);
+        }
+        img.rotate = new_rotate;
+        img.shape = new_shape;
     }
     static move_sands(grid) {
         for (let i = (data.grid_h-2); i >= 0; i--) {
@@ -128,6 +196,23 @@ class Utils {
             }
         }
     }
+    static pop_row(grid,i) {
+        for (let j = 0; j < data.grid_w; j++) {
+            // Actualizar id sans de todos los cuadrados y de las imagenes
+            grid[i][j].id
+            grid[i][j] = new Box(0,data.bg_color);
+        }
+    }
+    static verify_row(grid) {
+        let same_color = true;
+        for (let i = (data.grid_h-1); i >= 0; i--) {
+            for (let j = 0; j < data.grid_w; j++) {
+                if(grid[i][j].id != 0) {
+                    this.draw(i,j,this.grid[i][j].color);
+                }
+            }
+        }
+    }
 }
 
 class Grid {
@@ -135,12 +220,15 @@ class Grid {
         this.grid = new Array(data.grid_h);
         for (let i = 0; i < data.grid_h; i++) { this.grid[i] = new Array(data.grid_w).fill(new Box(0,data.bg_color)); }
     }
+    reset() {
+        this.grid = new Array(data.grid_h);
+        for (let i = 0; i < data.grid_h; i++) { this.grid[i] = new Array(data.grid_w).fill(new Box(0,data.bg_color)); }
+    }
     draw(i,j,color) {
         let y = (i - 4)*data.box_h;
         let x = j*data.box_w;
         ctx.fillStyle = color;
-        ctx.lineWidth = 1;  ctx.strokeStyle = data.line_color;
-        ctx.strokeRect(x,y,data.box_w,data.box_h);
+        ctx.fillRect(x,y,data.box_w,data.box_h);
     }
     clear(i,j) {
         this.draw(i,j,data.bg_color);
@@ -148,7 +236,9 @@ class Grid {
     drawAll() {
         for (let i = 0; i < data.grid_h; i++) {
             for (let j = 0; j < data.grid_w; j++) {
-                if(this.grid[i][j].id != 0) this.draw(i,j,this.grid[i][j].color);
+                if(this.grid[i][j].id != 0) {
+                    this.draw(i,j,this.grid[i][j].color);
+                }
             }
         }
     }
@@ -164,47 +254,126 @@ class Grid {
 class Game {
     constructor() {
         this.grid_controler = new Grid();
-        // solo para ids con sands falses
-        // Primero hacer caer los sands y luego los images
         this.imageStop = true;
         this.images = [];
         this.counter = 1;
         this.life = true;
+        this.score = 0;
     }
     reset() {
-        game.grid_controler.clearAll();
+        this.grid_controler.reset();
+        this.grid_controler.clearAll();
         this.imageStop = true;
         this.images = [];
         this.counter = 1;
         this.life = true;
+        this.score = 0;
+    }
+    frame() {
+        this.grid_controler.clearAll();
+        this.grid_controler.drawAll();
+    }
+    frameDead() {
+        this.grid_controler.clearAll();
+    }
+    move(key) {
+        if(this.counter != 1) {
+            if(key == "KeyA") {
+                if(Utils.verify_move_img(this.images[this.images.length-1],this.grid_controler.grid,-1,0)) {
+                    Utils.move_img(this.images[this.images.length-1],this.grid_controler.grid,-1,0);
+                    this.frame();
+                }
+            }
+            else if(key == "KeyD"){
+                if(Utils.verify_move_img(this.images[this.images.length-1],this.grid_controler.grid,1,0)) {
+                    Utils.move_img(this.images[this.images.length-1],this.grid_controler.grid,1,0);
+                    this.frame();
+                }
+            }
+            else if(key == "KeyW"){
+                if(Utils.verify_move_img(this.images[this.images.length-1],this.grid_controler.grid,0,-1)) {
+                    Utils.move_img(this.images[this.images.length-1],this.grid_controler.grid,0,-1);
+                    this.frame();
+                }
+            }
+            else if(key == "KeyS"){
+                if(Utils.verify_move_img(this.images[this.images.length-1],this.grid_controler.grid,0,1)) {
+                    Utils.move_img(this.images[this.images.length-1],this.grid_controler.grid,0,1);
+                    this.frame();
+                }
+            }
+            else if(key == "Space") {
+                if(Utils.verify_rotate_img(this.images[this.images.length-1],this.grid_controler.grid,1)) {
+                    Utils.rotate_img(this.images[this.images.length-1],this.grid_controler.grid,1);
+                }
+            }
+            else if(key == "KeyE") {
+                if(Utils.verify_rotate_img(this.images[this.images.length-1],this.grid_controler.grid,1)) {
+                    Utils.rotate_img(this.images[this.images.length-1],this.grid_controler.grid,1);
+                }
+            }
+            else if(key == "KeyR") {
+                if(Utils.verify_rotate_img(this.images[this.images.length-1],this.grid_controler.grid,-1)) {
+                    Utils.rotate_img(this.images[this.images.length-1],this.grid_controler.grid,-1);
+                }
+            }
+            else if(key == "Escape") {
+                // Button Music
+                if(sound.paused()) {
+                    sound.play();
+                }
+                else {
+                    sound.pause();
+                    sound.currentTime = 0; 
+                } 
+            }
+        }
     }
     loop() {
-        //this.grid_controler.clearAll();
-        if(this.imageStop) {
-            this.images.push(Utils.spawn_img(this.counter,this.grid_controler.grid));
-            this.imageStop = false;
-        }
-        //Utils.move_sands(this.grid_controler.grid);
-        console.log(this.grid_controler.grid);
-        for (let i = 0; i < this.images.length; i++) {
-            //console.log(this.images[i].y);
-            console.log(Utils.move_img(this.images[i],this.grid_controler.grid,0,1));
-        }
-        //this.grid_controler.drawAll();
-        this.counter++;     
+        if(this.life) {
+            if(this.imageStop) {
+                this.images.push(Utils.spawn_img(this.counter,this.grid_controler.grid));
+                this.imageStop = false;
+            }
+            Utils.move_sands(this.grid_controler.grid);
+            for (let i = 0; i < this.images.length; i++) {
+                if(Utils.verify_move_img(this.images[i],this.grid_controler.grid,0,1)) {
+                    Utils.move_img(this.images[i],this.grid_controler.grid,0,1);
+                }
+                else if(i == (this.images.length-1)) {
+                    if(this.images[i].y < 4) this.life = false;
+                    this.imageStop = true;
+                }
+            }
+            game.frame();
+            this.counter++; 
+        } 
+        else {
+            this.frameDead();
+        } 
     }
     
 }
 
+
 // Event Listeners
 let game = new Game();
-let loop = 0; let speed = 100;
+let loop = 0;
+let speed = 200;
 game.reset();
+document.addEventListener("keydown", (e)=>{
+    game.move(e.code);
+});
+
 startButton.addEventListener('click', () => {
-    game.loop();
-    /*
+    sound.play();
     loop = setInterval(() => {
         game.loop();
-    }, speed);*/
+    }, speed);
 });
-//resetButton.addEventListener('click', resetGame);
+resetButton.addEventListener('click', () => {
+    clearInterval(loop);
+    sound.pause();
+    sound.currentTime = 0;
+    game.reset();
+});
